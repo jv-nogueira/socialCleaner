c87b2ad3d9432509bd7c7b9ab7f5ad70255bc9b3
João Vitor Nogueira <jvnogueira2010@gmail.com>
2025-02-13 18:45:24 -0300

    Corrige erro ao cancelar prompt

--- content.js
+++ content.js
@@ -1,10 +1,14 @@
 let running = false;
 let arrayArmazenarNameAndUsername = [];
+let i=0
+let contadorUnfollow=0
+let limiteContador=0
+let answerTemp=0
 document.addEventListener("keydown", function(event) {
     if (event.keyCode === 113) {
         if (running) {
             running = false;
-            // Gera o arquivo ao pressionar F2 novamente
+            // Gera o arquivo por pressionar F2 novamente
             setTimeout(gerarArquivo,1000);
         } else {
             running = true;
@@ -13,96 +17,91 @@ document.addEventListener("keydown", function(event) {
     }
 })
 function iniciarBot(){
-    fetch(chrome.runtime.getURL('usernames.txt'))
-    .then(response => response.text())
-    .then(permitidosUsername => {
-        var standardMessage = "\n\nSupport: jvnogueira2010@gmail.com";
-        if(document.location.href.includes('followers')||document.location.href.includes('following')){
-            if(document.location.href.includes('followers')){
-                var promptUnfollow = 'remover dos seguidores' 
-            }else if(document.location.href.includes('following')){
-                var promptUnfollow = 'deixar de seguir (unfollow)' 
-            };
-            var question = prompt("Digite no campo abaixo a quantidade de pessoas que o bot vai "+promptUnfollow+":*"+
-            standardMessage)  
-        }else{alert("Para funcionar, precisa entrar no Instagram e clicar nos seguidores ou seguindo")};
-        
-        if(question > 0){
-            var questionTemp = prompt("Digite no campo abaixo quantos segundos quer que dure cada ciclo de intervalo de tempo para o unfollow. Lembrando que quanto maior o intervalo de tempo, mais seguidores o bot vai "+promptUnfollow+" sem ser impedido pelo instagram."+
+    var standardMessage = "\n\nSupport: jvnogueira2010@gmail.com";
+    if(document.location.href.includes('followers')||document.location.href.includes('following')){
+        var question = prompt("Digite no campo abaixo a quantidade de pessoas:*"+
+        standardMessage)  
+    }else{
+        alert("Precisa clicar nos seguidores ou seguindo do instagram")
+    };
+    if(question > 0){
+        var questionTemp = prompt("Digite no campo abaixo quantos segundos quer que dure cada ciclo:*"+
+        standardMessage)
+        if(!isNaN(questionTemp) && questionTemp > 9){
+            limiteContador=question
+            answerTemp=questionTemp/2
+            percorrer()
+        }else if(questionTemp < 10 && questionTemp != null){
+            alert('Digite um número igual ou maior a 10!'+
             standardMessage)
-            if(!isNaN(questionTemp) && questionTemp > 10){
-                // Pega a resposta do prompt e divide
-                var answerTemp = questionTemp/2
-                var i=0
-                var contadorUnfollow=0
-                percorrer()
-                function percorrer(){
-                    if (!running) return;
-                    // Referenciar cada perfil da lista
-                    var profileReference = [...document.querySelectorAll("span")].find(il => il.textContent.includes("Pesquisar")).parentElement.parentElement.parentElement.parentElement.parentElement.children[2].children[0].children[0].children[i] 
-                    if(i>0){
-                        // Rola a página para que o elemento fique vísivel 
-                        var viewProfile = [...document.querySelectorAll("span")].find(il => il.textContent.includes("Pesquisar")).parentElement.parentElement.parentElement.parentElement.parentElement.children[2].children[0].children[0].children[i-1].scrollIntoView() 
-                    };
-                    // Referencia o last index da lista de perfis
-                    var lastIndex = [...document.querySelectorAll("span")].find(il => il.textContent.includes("Pesquisar")).parentElement.parentElement.parentElement.parentElement.parentElement.children[2].children[0].children[0].children.length -1 
-                    // Visualiza o last element do index da lista de perfis
-                    var lastElementIndex = [...document.querySelectorAll("span")].find(il => il.textContent.includes("Pesquisar")).parentElement.parentElement.parentElement.parentElement.parentElement.children[2].children[0].children[0].children[lastIndex] 
-                    var getUsernameOrName = profileReference.children[0].children[0].children[0].children[1].children[0].children[0]
-                    // Pegar o nome da lista
-                    var getName = getUsernameOrName.children[1].children[0].innerText 
-                    // Pegar o username da lista
-                    var getUsername = getUsernameOrName.children[0].children[0].children[0].children[0].children[0].children[0].children[0].children[0].innerHTML 
-                    var getUsernameAndName = {getName, getUsername}
-                    var limiteContador = question;
-                    // Botão para deixar de seguir
-                    var buttonUnfollow = profileReference.children[0].children[0].children[0].children[2].children[0].children[0]  
-                        if(!permitidosUsername.includes(getUsername)){
-                            viewProfile 
-                            // Limite de usernames que o bot vai deixar de seguir
-                            if(contadorUnfollow < limiteContador){ 
-                                arrayArmazenarNameAndUsername.push(getUsernameAndName)
-                                setTimeout(() => {
-                                    buttonUnfollow.click()
-                                    setTimeout(() => {
-                                        [...document.querySelectorAll('button')].find(el => el.textContent == 'Remover'||el.textContent == 'Deixar de seguir').click()
-                                        contadorUnfollow++
-                                        i++
-                                        percorrer
-                                    },getRandomSeconds(answerTemp))
-                                },getRandomSeconds(answerTemp))
-                            }else{
-                                // Gera o arquivo ao chegar ao limite definido
-                                setTimeout(gerarArquivo,getRandomSeconds(1))
-                            };
-                        }else if(i < lastIndex){
-                            i++
-                            percorrer()
-                        }else{
-                            setTimeout(() => {
-                                lastElementIndex.scrollIntoView()
-                                setTimeout(() => {
-                                    percorrer()
-                                },getRandomSeconds(1))
-                            },getRandomSeconds(1))
-                        }
-                }
-            }else if(questionTemp <= 10){
-                alert('Digite um número acima de 10!'+
-                standardMessage)
-            }else if(typeof questionTemp == 'string'){
-                alert('Digite apenas números!'+
-                standardMessage)
-            }; 
-            }else if(typeof question == 'string' && question != 0){
-                alert('Digite apenas números!'+
-                standardMessage)
-            }else if(question == 0){
-                alert('Digite um número acima de 0!'+
-                standardMessage)
-            }; 
-    });
+            running = false;
+        }else if(typeof questionTemp == 'string' && questionTemp != null){
+            alert('Digite apenas números!'+
+            standardMessage)
+            running = false;
+        }else{
+            running = false;
+        }; 
+    }else if(typeof question == 'string' && question != 0){
+        alert('Digite apenas números!'+
+        standardMessage)
+        running = false;
+    }else if(question == 0){
+        alert('Digite um número acima de 0!'+
+        standardMessage)
+        running = false;
+    }else{
+        running = false;
+    }; 
 }
+
+function percorrer(){
+    try{
+        if (!running) return;
+        // Referenciar cada perfil da lista
+        var profileReference = [...document.querySelectorAll("span")].find(il => il.textContent.includes("Pesquisar")).parentElement.parentElement.parentElement.parentElement.parentElement.children[2].children[0].children[0].children
+        var getUsernameOrName = profileReference[i].children[0].children[0].children[0].children[1].children[0].children[0]
+        // Pegar o nome da lista
+        var getName = getUsernameOrName.children[1].children[0].innerText 
+        // Pegar o username da lista
+        var getUsername = getUsernameOrName.children[0].children[0].children[0].children[0].children[0].children[0].children[0].children[0].innerHTML 
+        var getUsernameAndName = {getName, getUsername}
+        // Botão para deixar de seguir
+        var buttonUnfollow = profileReference[i].children[0].children[0].children[0].children[2].children[0].children[0]  
+        fetch(chrome.runtime.getURL('usernames.txt'))
+        .then(response => response.text())
+        .then(permitidosUsername => {
+            if(!permitidosUsername.includes(getUsername)){
+                // Rola a página para que o elemento fique vísivel 
+                if(profileReference > 0){profileReference[i-1].scrollIntoView()}
+                // Limite de usernames que o bot vai deixar de seguir
+                if(contadorUnfollow < limiteContador){ 
+                        arrayArmazenarNameAndUsername.push(getUsernameAndName)
+                        setTimeout(() => {
+                            buttonUnfollow.click()
+                            setTimeout(() => {
+                                [...document.querySelectorAll('button')].find(el => el.textContent == 'Remover'||el.textContent == 'Deixar de seguir').click()
+                                contadorUnfollow++
+                                i++
+                                setTimeout(percorrer(),getRandomSeconds(1))
+                            },getRandomSeconds(answerTemp))
+                        },getRandomSeconds(answerTemp))
+                }else{
+                    // Gera o arquivo ao chegar ao limite definido
+                    setTimeout(gerarArquivo,getRandomSeconds(1))
+                };
+            }else{
+                i++
+                percorrer()
+            }
+        });
+    }catch{            
+        // Gera o arquivo em qualquer erro
+        setTimeout(gerarArquivo,getRandomSeconds(1))
+        running = false;
+    }
+}
+
 function gerarArquivo(){
     // Criar o conteúdo do CSV
     var csvContent = "\uFEFFNome\tUsername\n";
@@ -123,9 +122,9 @@ function gerarArquivo(){
     document.body.removeChild(link);
 }
 
-function getRandomSeconds(){
+function getRandomSeconds(sum){
     // Multiplica um random number 
-    let sec222 = Math.random() * 4; 
+    let sec222 = Math.random() * 4 + sum; 
     // Diminui para 3 casas decimais, converte de string para number e multiplica por 1000 que corresponde a 1 segundo
     sec222 =  parseFloat(sec222.toFixed(3))*1000; 
     // retorna o resultado aleatorio em segundos 
